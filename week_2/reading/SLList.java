d57050bc28c6226d5cae7f33583b9e3b5a9b224e
Steven Young <ywch2002@126.com>
2022-06-24 16:01:44 +0800

    add get() & longest() in SLList.java

--- week_2/reading/SLList.java
+++ week_2/reading/SLList.java
@@ -18,10 +18,11 @@ public class SLList<Type> {
     /* The first item (if it exists) is at sentinel.next*/
     private final TypeNode sentinel;
 
-    public SLList(){
+    public SLList() {
         sentinel = new TypeNode(null, null);
         size = 0;
     }
+
     public SLList(Type x) {
         sentinel = new TypeNode(null, null);
         sentinel.next = new TypeNode(x, null);
@@ -37,25 +38,48 @@ public class SLList<Type> {
         return sentinel.next.item;
     }
 
-    public void addLast(Type x){
+    public void addLast(Type x) {
         size++;
         TypeNode ptr = sentinel;
 
-        while (ptr.next != null){
+        while (ptr.next != null) {
             ptr = ptr.next;
         }
         ptr.next = new TypeNode(x, null);
     }
 
     public int size() {
-//        return size(first);
+        //        return size(first);
         return size;
     }
 
-//    private int size(TypeNode p){
-//        if (p.next == null) return 1;
-//        return 1 + size(p.next);
-//    }
+    //    private int size(TypeNode p){
+    //        if (p.next == null) return 1;
+    //        return 1 + size(p.next);
+    //    }
+
+    public Type get(int i) {
+        get(i, sentinel.next);
+    }
+    
+    public Type get(int i, TypeNode node) {
+        if (i == 0) {
+            return node.item;
+        }
+        return get(i - 1, node.next);
+    }
+    
+    public static String longest(SLList<String> list) {
+        int manDex = 0;
+        for (int i = 0; i < list.size(); i++) {
+            String longestString = list.get(manDex);
+            String thisString = list.get(i);
+            if (thisString.length() > longestString.length()) {
+                manDex = i;
+            }
+        }
+        return list.get(manDex);
+    }
 
     public static void main(String[] args) {
         SLList<Integer> L = new SLList<>(15);
