13e43c8247f5643662824c58580479754f55df62
Steven Young <ywch2002@126.com>
2022-06-28 17:03:53 +0800

    Task1: Deque Interface

---
+++ week_4/proj1b/ArrayDeque.java
@@ -0,0 +1,128 @@
+public class ArrayDeque<T> implements Deque<T>{
+    private T[] items;
+    private int left;
+    private int right;
+    private int capacity = 8;
+
+    public ArrayDeque(){
+        items = (T[]) new Object[capacity];
+        left = right = 0;
+    }
+
+    @Override
+    public void addFirst(T item){
+        if (isFull()) {
+            resize((int) (capacity * 1.5));
+        }
+        left = (left - 1 + capacity) % capacity;
+        items[left] = item;
+    }
+
+    @Override
+    public void addLast(T item){
+        if (isFull()) {
+            resize((int) (capacity * 1.5));
+        }
+        right = (right + 1) % capacity;
+        items[right] = item;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return ((right - left) == 0);
+    }
+
+    @Override
+    public int size(){
+        return (right - left + capacity) % capacity;
+    }
+
+    @Override
+    public void printDeque(){
+        if (left < right) {
+            for (int i = left; i < right; i++) {
+                if (i == right - 1) {
+                    System.out.println(items[i]);
+                    break;
+                }
+                System.out.print(items[i] + " ");
+            }
+        } else if (left > right) {
+            for (int i = left; i < capacity; i++) {
+                System.out.print(items[i] + " ");
+            }
+            for (int i = 0; i < right; i++) {
+                if (i == right - 1) {
+                    System.out.println(items[i]);
+                    break;
+                }
+                System.out.print(items[i] + " ");
+            }
+        }
+    }
+
+    @Override
+    public T removeFirst(){
+        if (isEmpty()) {return null;}
+        T tmp = items[left];
+        left = (left + 1) % capacity;
+        if (isLowUsageRate()) {
+            resize((int) (capacity * 0.5));
+        }
+        return tmp;
+    }
+
+    @Override
+    public T removeLast(){
+        if (isEmpty()) {return null;}
+        T tmp = items[right];
+        right = (right + 1) % capacity;
+        if (isLowUsageRate()) {
+            resize((int) (capacity * 0.5));
+        }
+        return tmp;
+    }
+
+    @Override
+    public T get(int index){
+        if (index < 0 || index >= size() || isEmpty()) {
+            return null;
+        }
+        if (left < right) {
+            return items[index + left];
+        } else {
+            return items[(index + left) % capacity];
+        }
+    }
+
+    private boolean isFull(){
+        return ((right - left) >= capacity);
+    }
+
+    private void resize(int newSize) {
+        T[] newArray = (T[]) new Object[newSize];
+
+        int size = size();
+        if (left < right) {
+            for (int i = left, j = 0; i < right && j < size; i++, j++) {
+                newArray[j] = items[i];
+            }
+        } else if (left > right) {
+            int j = 0;
+            for (int i = left; j < capacity - left; i++, j++) {
+                newArray[j] = items[i];
+            }
+            for (int i = 0; j < size; i++, j++) {
+                newArray[j] = items[i];
+            }
+        }
+        left = 0;
+        right = size;
+        items = newArray;
+        capacity = newSize;
+    }
+
+    private boolean isLowUsageRate() {
+        return capacity >= 16 && size() / (double) capacity < 0.25;
+    }
+}
---
+++ week_4/proj1b/LinkedListDeque.java
@@ -0,0 +1,108 @@
+
+public class LinkedListDeque<T> implements Deque<T>{
+    private class TNode{
+        TNode prev;
+        T item;
+        TNode next;
+
+        public TNode(TNode p, T i, TNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
+    }
+
+    private final TNode sentFront;
+    private final TNode sentBack;
+    private int size;
+
+    public LinkedListDeque(){
+        TNode first = new TNode(null, null, null);
+        size = 0;
+        sentFront = new TNode(null, null, null);
+        sentBack = new TNode(null, null, null);
+        sentFront.next = first;
+        sentBack.next = first;
+    }
+
+    public LinkedListDeque(T i){
+        TNode first = new TNode(null, i, null);
+        size = 1;
+        sentFront = new TNode(null, null, null);
+        sentBack = new TNode(null, null, null);
+        sentFront.next = first;
+        sentBack.next = first;
+    }
+
+    @Override
+    public void addFirst(T item){
+        size++;
+        sentFront.next = new TNode(null, item, sentFront.next);
+    }
+
+    @Override
+    public void addLast(T item){
+        size++;
+        sentBack.next = new TNode(sentBack.next, item, null);
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return sentFront.next.next == null;
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public void printDeque(){
+        for (TNode ptr = sentFront.next; ptr !=sentBack.next; ptr = ptr.next){
+            if (ptr.next == null){
+                System.out.println(ptr.item);
+                break;
+            }
+            System.out.println(ptr.item + " ");
+        }
+    }
+
+    @Override
+    public T removeFirst(){
+        size--;
+        if (sentFront.next == null) return null;
+        T tmp = sentFront.next.item;
+        sentFront.next = sentFront.next.next;
+        return tmp;
+    }
+
+    @Override
+    public T removeLast(){
+        size--;
+        if (sentBack.next == null) return null;
+        T tmp = sentBack.next.item;
+        sentBack.next = sentBack.next.prev;
+        return tmp;
+    }
+
+    @Override
+    public T get(int index){
+        TNode ptr = sentFront.next;
+        for(int i=0; i<index; i++){
+            ptr = ptr.next;
+        }
+        return ptr.item;
+    }
+
+    public T getRecursive(int index){
+        if (size < index){
+            return null;
+        }
+        return getRecursive(sentFront.next, index);
+    }
+
+    public T getRecursive(TNode node, int i){
+        if (i == 0) return node.item;
+        return getRecursive(node.next, i-1);
+    }
+}
